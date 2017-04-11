5f8418d551a07e46e463a9a4b7a845dd281ba41d
Rainbow Phoenix <Rainbowdashtheone@gmail.com>
2017-04-11 17:33:40 +0200

    Revert "Revert "index test""
    This reverts commit 77ee6b89e3eacc467684bd9f415862ad5af6419c.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <title>imgBoard - Acceuil</title>
+    <link rel="prefetch" href="index.php">
+
+  </head>
+  <body>
+	<section id="intro" class="intro-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Welcome Here !</h1>
+                    <h4><p>bla bla.</p>more bla bla.<p></p>
+                    <a class="btn btn-default page-scroll" href="#">Shut up!</a></h4>
+                </div>
+            </div>
+        </div>
+    </section>
+  </body>
+</html>
